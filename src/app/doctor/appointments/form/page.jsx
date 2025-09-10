6c3bbf7b99a3e464f132954cdd569f432e2bc75a
Kooru Mahey <koorumahey01@gmail.com>
2025-09-10 16:36:35 +0530

    Add AppointmentFormPage component for creating new appointments

---
+++ src/app/doctor/appointments/form/page.jsx
@@ -0,0 +1,9 @@
+function AppointmentFormPage() {
+  return (
+    <div>
+      <h1>Create a New Appointment</h1>
+      {/* Appointment form goes here */}
+    </div>
+  );
+}
+export default AppointmentFormPage;
